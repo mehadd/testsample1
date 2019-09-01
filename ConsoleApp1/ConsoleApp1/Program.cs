7c7d661ce5a0fb6afd4222f27c693dc1352a579f
mehadd <mehad.araby@gmail.com>
2019-09-02 00:01:25 +0200

    add all files

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = 0;
+            int y = 6;
+            int z = y / x;
+            Console.WriteLine(z);
+        }
+    }
+}
